e45bcc463a2afe598c5468c700a5d4674eee89ad
Diego Archila <daat3523@gmail.com>
2021-12-23 03:22:00 -0500

    [ADD] 404.gif, PaginaEnConstruccion.jpg, favicon.png 404.html, login.html, register.html, inconstruction.html [UPDATE] home.html: Se mejora el home, app.js: Se agregan las nuevas rutas con relacion a los html agregados. styles.css: se agregan estilos a las nuevos html agregados.

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ const publicPath=path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "./views/home.html")));
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.get("/cart", (req,res) => res.sendFile(path.resolve(__dirname, "./views/inconstruction.html")));
+app.get("/offers", (req,res) => res.sendFile(path.resolve(__dirname, "./views/inconstruction.html")));
+app.get("/officialstores", (req,res) => res.sendFile(path.resolve(__dirname, "./views/inconstruction.html")));
+app.get("/sell", (req,res) => res.sendFile(path.resolve(__dirname, "./views/inconstruction.html")));
+app.get("/help", (req,res) => res.sendFile(path.resolve(__dirname, "./views/inconstruction.html")));
+app.get("/*", (req,res) => res.sendFile(path.resolve(__dirname, "./views/404.html")));
 
 app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
